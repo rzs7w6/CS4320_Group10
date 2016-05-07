0c32ab989441ab9630776518c81569d7d12ba34e
Colin Stevens <mail@colinjstevens.com>
2016-05-07 22:38:33 +0000

    Updated user detail UI

--- user_detail.php
+++ user_detail.php
@@ -1,92 +1,156 @@
-
-
-
-<style>
-    .div{
-        text-align: right;
-
-    }
-    a{
-        margin-right: 20px;
-        text-decoration: none;
-    }
-</style>
-<div class="div">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Super LinkedIn</title>
+    <link rel="stylesheet" type="text/css" href="css/index2.css"/>
+    <link rel="stylesheet" type="text/css" href="css/forms.css"/>
+    <link rel="stylesheet" type="text/css" href="css/semantic.min.css"/>
+</head>
+<body>
     <?php
-    echo "<a href=\"contact_user.php?u_id=$_GET[u_id]\">Send notice</a> | <a href=\"index.php\">Home</a>";
-    ?>
-</div>
+    require_once ("SqlUtiles.php");
 
-<?php
-/**
- * Created by PhpStorm.
- * User: baidu
- * Date: 16/4/4
- * Time: 23:49
- */
+    if(empty($_GET['u_id'])){
+        if(empty($_POST['user_name'])||empty($_POST['password'])){
+            echo "<script>alert('User ID cannot be empty');parent.location.href='index.php'; </script>";
+            exit;
+        }
+    }
 
+    $util=new SqlUtils();
 
-require_once ("SqlUtiles.php");
+    $res=$util->query_user_info($_GET['u_id']);
 
-if(empty($_GET['u_id'])){
-    if(empty($_POST['user_name'])||empty($_POST['password'])){
-        echo "<script>alert('User ID cannot be empty');parent.location.href='index.php'; </script>";
+    if($res->rowCount()>0){
+        $res=$res->fetch();
+    } else {
+        header("Location:add_user_info.html");
         exit;
     }
-}
-
-$util=new SqlUtils();
-
-$res=$util->query_user_info($_GET['u_id']);
-
-if($res->rowCount()>0){
-
-
-    $res=$res->fetch();
-    echo "<div>User Name <input type='text' name='real_name' value='$res[real_name]'></div>";
-    echo "<div>Job Name <input type='text' name='w_name' value='$res[w_name]'></div>
-        <div>Job space <input type='text' name='w_space' value='$res[w_space]'></div>
-        <div>School space <input type='text' name='s_space' value='$res[s_space]'></div>
-        <div>School Name <input type='text' name='s_name' value='$res[s_name]'></div>
-        <div>School Start date <input type='date' name='s_s_date' value='$res[s_s_date]'></div>
-        <div>School End date <input type='date' name='s_e_date' value='$res[s_e_date]'></div>
-        <div>Experience Job name <input type='text' name='e_w_name' value='$res[e_w_name]'></div>
-        <div>Experience Job Space <input type='text' name='e_w_space' value='$res[e_w_space]'></div>
-        <div>Experience Start date <input type='date' name='e_s_date' value='$res[e_s_date]'></div>
-        <div>Experience end date <input type='date' name='e_e_date' value='$res[e_e_date]'></div>
-        <div>Languages";
-
-    if($res['languages']=='english'){
-        echo "<input type='languages' name='e_e_date' value='english'>";
-    }
-    else
-    {
-        echo "<input type='languages' name='e_e_date' value='chinese'>";
-    }
-
-
-    echo "</div><div>Degree";
-
-    if($res['degree']=='below college'){
-        echo "<input  name='degree' value='below college'>";
-    }
-    else if($res['degree']=='undergradute'){
-        echo "<input  name='degree' value='undergradute'>";
-    }
-    else if($res['degree']=='Master'){
-        echo "<input  name='degree' value='Master'>";
-    }
-    else{
-        echo "<input  name='degree' value='Phd'>";
-    }
-
-
-        echo "</div><div>Skills <textarea rows='20' cols='70' name='skills'>$res[skills]</textarea></div>";
-
-        echo "<div>Telphone <input type='tel' name='tel' value='$res[tel]'></div>";
-
-}
-else{
-    header("Location:add_user_info.html");
-    exit;
-}
+    ?>
+    <div class="ui fixed menu">
+        <div class="header item">Super LinkedIn</div>
+        <div class="right menu">
+            <a class="item" href="index.php">Home</a>
+            <a class="item" href="logout.php">Logout</a>
+        </div>
+    </div>
+    <div id="main_content">
+    <div class="ui segment" id="application">
+        <h2 class="ui header">User Application</h2>
+        <form class="ui form">
+        <div class="field">
+            <label>Username</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[real_name].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Job Title</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[w_name].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Job Space</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[w_space].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>School space</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[s_space].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>School name</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[s_name].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>School Start Date</label>
+            <?php
+            echo '<input type="date" name="first-name" value="'.$res[s_s_date].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>School End Date</label>
+            <?php
+            echo '<input type="date" name="first-name" value="'.$res[s_e_date].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Experience Job Name</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[e_w_name].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Experience Job Space</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[e_w_space].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Experience Start Date</label>
+            <?php
+            echo '<input type="date" name="first-name" value="'.$res[e_s_date].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Experience End Date</label>
+            <?php
+            echo '<input type="date" name="first-name" value="'.$res[e_e_date].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Language</label>
+            <?php
+            echo '<input type="text" name="first-name" value="'.$res[languages].'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Degree</label>
+            <?php
+            $val = '';
+            if($res['degree']=='below college') {
+                $val = 'Less than college';
+            } else if($res['degree']=='undergradute') {
+                $val = 'Undergraduate';
+            } else if($res['degree']=='Master') {
+                $val = 'Master';
+            } else {
+                $val = 'PhD';
+            }
+            echo '<input type="text" name="first-name" value="'.$val.'" disabled class="disabled">';
+            ?>
+        </div>
+        <div class="field">
+            <label>Skills</label>
+            <textarea rows='20' cols='70' name='skills' disabled>
+            <?php echo($res[skills]); ?>
+            </textarea>
+        </div>
+        <div class="field">
+            <label>Telephone</label>
+            <?php
+            echo '<input type="tel" name="first-name" value="'.$res[tel].'" disabled class="disabled">';
+            ?>
+        </div>
+        </form>
+    </div>
+    <div class="ui segment">
+        <div class="ui two column grid">
+            <div class="column">
+                <a class="ui large fluid primary button" href="contact_user.php?u_id=<?php echo($_GET[u_id]);?>">Contact applicant</a>
+            </div>
+            <div class="column">
+                <a class="ui large fluid button" href="javascript:history.back()">Go back</a>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
